c3dc62a66cdfc78977cc2add21bfb5509c4839da
Jason <jason@funktroniclabs.com>
2023-04-13 11:02:24 -0700

    Click to change color, woooo

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <SDL.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(int argc, char* argv[])
 {
-    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Init(SDL_INIT_TIMER | SDL_INIT_VIDEO | SDL_INIT_EVENTS);
 
     SDL_Window* win = SDL_CreateWindow("GAME",
         SDL_WINDOWPOS_CENTERED,
@@ -10,12 +12,12 @@ int main(int argc, char* argv[])
         640, 640, 0);
 
     SDL_Renderer* renderer = SDL_CreateRenderer(win, -1, 0);
+    int r = 255;
+    int g = 0;
+    int b = 255;
 
-    SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
-
-    SDL_RenderClear(renderer);
-
-    SDL_RenderPresent(renderer);
+    time_t t;
+    srand((unsigned) time(&t));
 
     bool quit = false;
     while (!quit)
@@ -24,8 +26,20 @@ int main(int argc, char* argv[])
         while (SDL_PollEvent(&event))
         {
             if (event.type == SDL_QUIT)
+            {
                 quit = true;
+            }
+            else if (event.type == SDL_MOUSEBUTTONDOWN)
+            {
+                r = rand() % 256;
+                g = rand() % 256;
+                b = rand() % 256;
+            }
         }
+        
+        SDL_SetRenderDrawColor(renderer, r, g, b, 255);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
     }
 
     SDL_Quit();
